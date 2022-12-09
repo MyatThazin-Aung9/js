1dca4cfda716293ac191b1c8c02b24ba1298ec55
Myat Thazin Aung <myatthazinaung.mtu@gmail.com>
2022-12-09 18:09:38 +0630

    add validation

--- js/src/cal.js
+++ js/src/cal.js
@@ -1,33 +1,74 @@
 let btn = document.getElementById("btncalculate");
 let ansresult = document.getElementById("result");
+let numOneError =document.getElementById("numOneError");
+let numTwoError =document.getElementById("numTwoError");
+let operatorError =document.getElementById("opeartorError");
 let finalResult;
+let numOneStatus,
+    numTwoStatus,
+    operatorStatus = false;
+
+    numOneError.style.display = "none";
+    numTwoError.style.display = "none";
+    operatorError.style.display = "none";
 
 btn.addEventListener("click",function(){
     let num1 = document.getElementById("firstNum");
     let num2 = document.getElementById("secNum");
     let operator = document.getElementById("operation");
-    num1 = parseInt(num1.value);
-    num2 = parseInt(num2.value);
 
-    console.log(typeof num1);
-    console.log(typeof num2);
-    switch(operator.value){
-        case "add":
-            finalResult = num1 + num2;
-            break;
-        case "minus":
-            finalResult = num1 - num2;
-            break;
-        case "multiply":
-            finalResult = num1 * num2;
-            break;
-        case "division":
-            finalResult = num1 / num2;
-            break;
-        case "default":
-            console.log("Operation error");
-            break;
+    checkValidation(num1, num2, operator);
+
+    if (numOneStatus == false && numTwoStatus == false && operatorStatus == false) {
+        num1 = parseInt(num1.value);
+        num2 = parseInt(num2.value);
+
+        console.log(typeof num1);
+        console.log(typeof num2);
+        switch(operator.value){
+            case "add":
+                finalResult = num1 + num2;
+                break;
+            case "minus":
+                finalResult = num1 - num2;
+                break;
+            case "multiply":
+                finalResult = num1 * num2;
+                break;
+            case "division":
+                finalResult = num1 / num2;
+                break;
+            case "default":
+                console.log("Operation error");
+                break;
+        }
+        console.log(finalResult);
+        ansresult.innerHTML = finalResult;
     }
-    console.log(finalResult);
-    ansresult.innerHTML = finalResult;
 });
+
+function checkValidation(num1, num2, operator) {
+    if (num1.value == "" || num1.value == null || num1.value == undefined) {
+        numOneError.style.display = "block";
+        numOneStatus = true;
+    }else{
+        numOneError.style.display = "none";
+        numOneStatus = false;
+    }
+
+    if (num2.value == "" || num2.value == null || num2.value == undefined) {
+        numTwoError.style.display = "block";
+        numTwoStatus = true;
+    }else{
+        numTwoError.style.display = "none";
+        numTwoStatus = false;
+    }
+
+    if (operator.value == "empty") {
+        operatorError.style.display = "block";
+        operatorStatus = true;
+    }else{
+        operatorError.style.display = "none";
+        operatorStatus = false;
+    }
+}
